85a486eb795891294c2be328e236c4b585b8e298
luunch88 <luunch88@gmail.com>
2026-05-01 15:40:44 +0200

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -4,16 +4,18 @@ const ALLOWED_ORIGINS = new Set([
   'http://localhost:3000',
   'http://localhost:5173',
   'http://localhost:8080',
+  'http://localhost:8765',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:8080'
+  'http://127.0.0.1:8080',
+  'http://127.0.0.1:8765'
 ]);
 
 export function applyCors(req, res, methods) {
   const origin = req.headers.origin;
   res.setHeader('Vary', 'Origin');
   res.setHeader('Access-Control-Allow-Methods', methods);
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-admin-secret');
 
   if (!origin) return true;
   if (!ALLOWED_ORIGINS.has(origin)) return false;
